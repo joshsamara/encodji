7d85d6eaf60d815f61790c076992835568ef46ba
Josh Samara <joshsamara@gmail.com>
2018-05-21 13:13:04 -0400

    Handle multi-character emojis

--- encodji/encoders.py
+++ encodji/encoders.py
@@ -5,16 +5,30 @@ Encoders and decoding utilities.
 EMOJIS = [
     '\U0001F947',
     '\U0001F948',
-    '\U0001F949'
+    '\U0001F949',
+    # Test for multi-character emojis
+    '\U0001F3F4\U000E0067\U000E0062\U000E0065\U000E006E\U000E0067\U000E007F'
 ]
 SPACE = len(EMOJIS)
 SPECIAL_INDICATOR = '!'
+MULTI_INDICATOR = ' '
+
+
+def _encode_simple(index):
+    emoji = EMOJIS[index]
+    if len(emoji) > 1:
+        return '{indicator}{emoji}{indicator}'.format(
+            indicator=MULTI_INDICATOR,
+            emoji=emoji
+        )
+    return emoji
 
 
 def _encode_special(code):
     emojis = []
     while code > 0:
-        emojis.append(EMOJIS[code % SPACE])
+        encode_index = code % SPACE
+        emojis.append(_encode_simple(encode_index))
         code //= SPACE
     return "{indicator}{emojis}{indicator}".format(
         indicator=SPECIAL_INDICATOR,
@@ -22,10 +36,20 @@ def _encode_special(code):
     )
 
 
-def _decode_special(emojis):
+def _decode_special(chars):
     exp = 0
     total = 0
-    for emoji in emojis:
+    i = 0
+    while i < len(chars):
+        emoji_ref = chars[i]
+        if emoji_ref == MULTI_INDICATOR:
+            # Special encoded emojis may also contain multi-emojis
+            matching_idx, multi_chars = _find_next(chars, i, MULTI_INDICATOR)
+            i = matching_idx + 1
+            emoji = ''.join(multi_chars)
+        else:
+            emoji = emoji_ref
+            i += 1
         total += EMOJIS.index(emoji) * SPACE ** exp
         exp += 1
     return chr(total)
@@ -72,22 +96,27 @@ def generate_decoded(encoded):
     # TODO: we need a thing for multi-character emojis
     while i < len(encoded):
         c = encoded[i]
-        if c != '!':
-            # If we have a regular characet
-            yield _decode_simple(c)
-            i += 1
-        else:
+        if c == SPECIAL_INDICATOR:
             # Grab a set of emojis up to the next '!' to decode
-            matching_idx, emojis = _find_next(encoded, i, '!')
-            yield _decode_special(emojis)
+            matching_idx, chars = _find_next(encoded, i, SPECIAL_INDICATOR)
             i = matching_idx + 1
+            yield _decode_special(chars)
+        elif c == MULTI_INDICATOR:
+            # We have a multi-character emoji
+            matching_idx, chars = _find_next(encoded, i, MULTI_INDICATOR)
+            i = matching_idx + 1
+            yield _decode_simple(chars)
+        else:
+            # If we have a simple encoding, simple decode it
+            i += 1
+            yield _decode_simple(c)
 
 
 def decode_string(encoded):
     return ''.join(generate_decoded(encoded))
 
+
 original = 'Thîs is Tôtally a TéS†®.'
 encoded = encode_string(original)
-print(encoded)
 decoded = decode_string(encoded)
 assert original == decoded
